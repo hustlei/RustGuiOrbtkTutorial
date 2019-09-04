fa1fbe43f7362d467f8e00ce894c68301fa564d8
hustlei <hustlei@sina.cn>
2019-09-04 21:15:08 +0800

    update helloword to 2019.9 api

--- src/bin/helloworld.rs
+++ src/bin/helloworld.rs
@@ -1,38 +1,19 @@
+// author: lileilei
+// lastedited: 2019.9
+
 //! helloworld for rust orbtk
 
-use orbtk::*;
-
-// 分解步骤版helloworld
-/*
-fn main() {
-    let mut application = Application::default();
-    //创建一个textblock，添加一个Label类型的属性
-    let text_block = TextBlock::create().text("Hello, World!");
-    //创建root控件，把textBlock设置为子控件
-    let root = Template::default().child(text_block);
-
-    application
-        .create_window()
-        .bounds(Bounds::new(100.0, 100., 320., 210.))
-        .title("OrbTk - helloworld")
-        .resizable(true) //可以调整窗口大小
-        .root(root)
-        .build();
-    application.run();
-}
-*/
+use orbtk::prelude::*;
 
 fn main() {
-    let mut application = Application::default();
-
-    let root = Template::default().child(TextBlock::create().text("Hello, World!"));
-
-    application
-        .create_window()
-        .bounds((100.0, 100., 320., 210.))
-        .title("OrbTk - helloworld")
-        .resizable(true)
-        .root(root)
-        .build();
-    application.run();
+    Application::new()
+        .window(|ctx| {
+            Window::create()
+                .title("OrbTk - helloworld")
+                .position((100.0, 100.0))
+                .size(300.0, 200.0)
+                .child(TextBlock::create().text("Hello World!").build(ctx))
+                .build(ctx)
+        })
+        .run();
 }
